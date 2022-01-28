ab5efb8c3a98affe56d311a8f8a6c108bae942d4
William Verde <williamverde10@gmail.com>
2022-01-28 11:45:19 -0400

    Added: Validation when provider has a bid received

--- Services/BidService.php
+++ Services/BidService.php
@@ -33,7 +33,15 @@ class BidService
         if(is_null($auction))
             throw new \Exception('Auction not Found or status Inactive', 500);
 
-        
+        //Validate if user doen't have Bids Actives
+        $bids = $this->bid->findByAttributes([
+            'provider_id'=> $data['provider_id'],
+            'status' => 1 //RECEIVED = 1;
+        ]);
+        if(!is_null($bids))
+            throw new \Exception('User has other bid received for this auction', 500);
+
+
         // Search if Auction category has a Bid Service to Calculate Points(Score)
         if(is_null($auction->category->bid_service)){
             $points = $data['amount'];
