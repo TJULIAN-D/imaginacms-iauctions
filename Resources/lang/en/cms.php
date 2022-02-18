551d3e13450f1df98a2844dc244aa5d79f72c907
msolano <msolano@imaginacolombia.com>
2022-02-17 20:34:37 -0500

    Add: cms translations

---
+++ Resources/lang/en/cms.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Auctions",
+    "adminCategories" => "Categories",
+    "adminAuctions" => "Auctions",
+    "adminBids" => "Bids",
+  ],
+
+  "newCategory" => "New Category",
+  "updateCategory" => "Update Category",
+  "newAuction" => "Create auction",
+  "updateAuction" => "Update auction",
+  "newBid" => "New Bid",
+  "updateBid" => "Update Bid",
+
+  "auctionForm" => "Auction Form",
+  "bidForm" => "Bid Form",
+  "activeState" => "Active",
+  "inActiveState" => "InActive",
+  "finishedState" => "Finished",
+  "typeOpen" => "Open",
+  "typeInverse" => "Inverse",
+  "bidUp" => "Bid Up"
+];
---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Licitaciones",
+    "adminCategories" => "Categorias",
+    "adminAuctions" => "Licitaciones",
+    "adminBids" => "Pujas",
+  ],
+
+  "newCategory" => "Nueva Categoría",
+  "updateCategory" => "Actualizar Categoría",
+  "newAuction" => "Nueva licitación",
+  "updateAuction" => "Editar licitación",
+  "newBid" => "Nueva Puja",
+  "updateBid" => "Actualizar Puja",
+
+  "auctionForm" => "Formulario para Licitación",
+  "bidForm" => "Formulario para Puja",
+  "activeState" => "Activo",
+  "inActiveState" => "Inactivo",
+  "finishedState" => "Culminado",
+  "typeOpen" => "Abierto",
+  "typeInverse" => "Inverso",
+  "bidUp" => "Pujar"
+];
