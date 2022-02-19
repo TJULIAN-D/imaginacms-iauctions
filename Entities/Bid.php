582d1b8db23c74dcd036541f323832708b8264f4
Juan Escalante <jcec007@gmail.com>
2022-02-19 13:55:23 -0500

    Update: Bid Entity | added winner relation

--- Entities/Bid.php
+++ Entities/Bid.php
@@ -13,60 +13,66 @@ use Modules\Iauctions\Traits\Notificable;
 
 class Bid extends CrudModel
 {
-   
-    use isFillable, Notificable;
-
-    protected $table = 'iauctions__bids';
-    public $transformer = 'Modules\Iauctions\Transformers\BidTransformer';
-    public $requestValidation = [
-        'create' => 'Modules\Iauctions\Http\Requests\CreateBidRequest',
-        'update' => 'Modules\Iauctions\Http\Requests\UpdateBidRequest',
-      ];
-   
-    protected $fillable = [
-        'auction_id',
-        'provider_id',
-        'description',
-        'amount',
-        'points',
-        'status',
-        'winner',
-        'options'
-    ];
-
-    protected $casts = ['options' => 'array'];
-
-    
-    //============== RELATIONS ==============//
-
-    public function auction()
-    {
-        return $this->belongsTo(Auction::class);
-    }
-
-    public function provider()
-    {
-        $driver = config('asgard.user.config.driver');
-        return $this->belongsTo("Modules\\User\\Entities\\{$driver}\\User");
-    }
-
-    //============== MUTATORS / ACCESORS ==============//
-    
-    public function setOptionsAttribute($value)
-    {
-        $this->attributes['options'] = json_encode($value);
-    }
-
-    public function getOptionsAttribute($value)
-    {
-        return json_decode($value);
-    }
-
-    public function getStatusNameAttribute()
-    {
-        $status = new StatusBid();
-        return $status->get($this->status);
-    }
-
-    
+  
+  use isFillable, Notificable;
+  
+  protected $table = 'iauctions__bids';
+  public $transformer = 'Modules\Iauctions\Transformers\BidTransformer';
+  public $requestValidation = [
+    'create' => 'Modules\Iauctions\Http\Requests\CreateBidRequest',
+    'update' => 'Modules\Iauctions\Http\Requests\UpdateBidRequest',
+  ];
+  
+  protected $fillable = [
+    'auction_id',
+    'provider_id',
+    'description',
+    'amount',
+    'points',
+    'status',
+    'winner',
+    'options'
+  ];
+  
+  protected $casts = ['options' => 'array'];
+  
+  
+  //============== RELATIONS ==============//
+  
+  public function auction()
+  {
+    return $this->belongsTo(Auction::class);
+  }
+  
+  public function provider()
+  {
+    $driver = config('asgard.user.config.driver');
+    return $this->belongsTo("Modules\\User\\Entities\\{$driver}\\User");
+  }
+  
+  public function winner()
+  {
+    $driver = config('asgard.user.config.driver');
+    return $this->belongsTo("Modules\\User\\Entities\\{$driver}\\User", "winner");
+  }
+  
+  //============== MUTATORS / ACCESORS ==============//
+  
+  public function setOptionsAttribute($value)
+  {
+    $this->attributes['options'] = json_encode($value);
+  }
+  
+  public function getOptionsAttribute($value)
+  {
+    return json_decode($value);
+  }
+  
+  public function getStatusNameAttribute()
+  {
+    $status = new StatusBid();
+    return $status->get($this->status);
+  }
+  
+  
 }
