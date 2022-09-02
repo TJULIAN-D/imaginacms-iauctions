dc49e929d2b811e43f921619cb00c387edf2b8ab
William Verde <williamverde10@gmail.com>
2022-09-02 09:48:58 -0400

    Added: exports bids

--- Config/config.php
+++ Config/config.php
@@ -8,4 +8,17 @@ return [
   	*/
   	'hourFormat' => 'd-m-Y H:i A',
 
+  	/*
+  	|--------------------------------------------------------------------------
+  	| Define all the exportable available
+  	|--------------------------------------------------------------------------
+  	*/
+  	'exportable' => [
+	    "bids" => [
+	      'moduleName' => "Iauctions",
+	      'fileName' => "Bids",
+	      'exportName' => "BidsExport"
+	    ]
+  	],
+
 ];
