b70f70104df23cf8bc61cacc67ec3d458c9d61b8
Juan Escalante <jcec007@gmail.com>
2022-10-10 18:29:08 -0500

    first tenant features

--- Providers/IauctionsServiceProvider.php
+++ Providers/IauctionsServiceProvider.php
@@ -48,7 +48,7 @@ class IauctionsServiceProvider extends ServiceProvider
         $this->mergeConfigFrom($this->getModuleConfigFilePath('iauctions', 'cmsPages'), "asgard.iauctions.cmsPages");
         $this->mergeConfigFrom($this->getModuleConfigFilePath('iauctions', 'cmsSidebar'), "asgard.iauctions.cmsSidebar");
 
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        //$this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 
     /**
